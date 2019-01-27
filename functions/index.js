8978c524edea697ce72116c2490c467225b3ddcd
Yadunandan Batchu <nandubatchu@gmail.com>
2019-01-27 18:43:22 +0530

    first cut lit-sink

---
+++ functions/index.js
@@ -0,0 +1,194 @@
+const functions = require('firebase-functions')
+const fs = require('fs')
+const App = require('@octokit/app')
+const request = require('@octokit/request')
+const cors = require('cors')({origin: true})
+const cert = functions.config() ? functions.config().testservice.key : fs.readFileSync(__dirname+'/../github_app.pem')
+
+let owner = 'nandubatchu'
+let repo = 'lit-sink'
+const APP_ID = 24297
+
+let saveFileToRepo = (contentString, commitMessage, filePath, userData) => {
+    let path = filePath
+    let branch = 'master'
+    let committer = author = userData
+
+    // Initialising the File content
+    let content = contentString
+    let message = commitMessage
+    
+    const app = new App({ id: APP_ID, privateKey: cert })
+    const jwt = app.getSignedJsonWebToken()
+
+    let call_installation_data = () => {
+        return request('GET /repos/:owner/:repo/installation', {
+            owner: owner,
+            repo: repo,
+            headers: {
+                authorization: `Bearer ${jwt}`,
+                accept: 'application/vnd.github.machine-man-preview+json'
+            }
+        }).then(res => res.data)
+    }
+
+    let call_save_blob_data = (installationAccessToken) => {
+        return request('POST /repos/:owner/:repo/git/blobs', {
+            owner: owner,
+            repo: repo,
+            content: content,
+            headers: {
+                authorization: `token ${installationAccessToken}`,
+                accept: 'application/vnd.github.machine-man-preview+json'
+            }
+        }).then(res => res.data)
+    }
+
+    let call_get_blob_data = (installationAccessToken, save_blob_data) => {
+        return request('GET /repos/:owner/:repo/git/blobs/:file_sha', {
+            owner: owner,
+            repo: repo,
+            file_sha: save_blob_data.sha,
+            headers: {
+                authorization: `token ${installationAccessToken}`,
+                accept: 'application/vnd.github.machine-man-preview+json'
+            }
+        }).then(res => res.data)
+    }
+
+    let call_create_file_data = (installationAccessToken, get_blob_data) => {
+        return request('PUT /repos/:owner/:repo/contents/:path', {
+            owner: owner, 
+            repo: repo, 
+            path: path, 
+            message: message, 
+            content: get_blob_data.content, 
+            branch: branch, 
+            committer: committer, 
+            author: author,
+            headers: {
+                authorization: `token ${installationAccessToken}`,
+                accept: 'application/vnd.github.machine-man-preview+json'
+            }
+        }).then(res => res.data)
+    }
+
+    return call_installation_data()
+        .then(installation_data => {
+            console.log("installation_data", installation_data)
+            const installationId = installation_data ? installation_data.id : INSTALLATION_ID
+            console.log("installationId:", installationId)
+            app.getInstallationAccessToken({ installationId })
+                .then(installationAccessToken => {
+                    call_save_blob_data(installationAccessToken)
+                        .then(save_blob_data => {
+                            console.log("save_blob_data", save_blob_data)
+                            call_get_blob_data(installationAccessToken, save_blob_data)
+                                .then(get_blob_data => {
+                                    console.log("get_blob_data", get_blob_data)
+                                    call_create_file_data(installationAccessToken, get_blob_data)
+                                        .then(create_file_data => {
+                                            console.log("create_file_data", create_file_data)
+                                            return create_file_data.content.html_url
+                                        })
+                                        .catch(err => {
+                                            console.log(err)
+                                            return undefined
+                                        })
+                                })
+                        })
+                })
+        })
+
+    // const { data: installation_data } = await request('GET /repos/:owner/:repo/installation', {
+    //     owner: owner,
+    //     repo: repo,
+    //     headers: {
+    //       authorization: `Bearer ${jwt}`,
+    //       accept: 'application/vnd.github.machine-man-preview+json'
+    //     }
+    //   })
+    // console.log("installation_data", installation_data)
+    
+    // const installationId = installation_data ? installation_data.id : INSTALLATION_ID
+    // console.log("installationId:", installationId)
+    // const installationAccessToken = await app.getInstallationAccessToken({ installationId })
+
+    
+
+    // const { data: save_blob_data } = await request('POST /repos/:owner/:repo/git/blobs', {
+    //     owner: owner,
+    //     repo: repo,
+    //     content: content,
+    //     headers: {
+    //       authorization: `token ${installationAccessToken}`,
+    //       accept: 'application/vnd.github.machine-man-preview+json'
+    //     }
+    //   })
+
+    // console.log("save_blob_data", save_blob_data)
+
+    // const { data: get_blob_data } = await request('GET /repos/:owner/:repo/git/blobs/:file_sha', {
+    //     owner: owner,
+    //     repo: repo,
+    //     file_sha: save_blob_data.sha,
+    //     headers: {
+    //       authorization: `token ${installationAccessToken}`,
+    //       accept: 'application/vnd.github.machine-man-preview+json'
+    //     }
+    //   })
+
+    // console.log("get_blob_data", get_blob_data)
+
+    // try {
+    //     const { data: create_file_data} = await request('PUT /repos/:owner/:repo/contents/:path', {
+    //         owner: owner, 
+    //         repo: repo, 
+    //         path: path, 
+    //         message: message, 
+    //         content: get_blob_data.content, 
+    //         branch: branch, 
+    //         committer: committer, 
+    //         author: author,
+    //         headers: {
+    //           authorization: `token ${installationAccessToken}`,
+    //           accept: 'application/vnd.github.machine-man-preview+json'
+    //         }
+    //       })
+        
+    //     console.log("create_file_data", create_file_data)
+    //     return create_file_data.content.html_url
+
+    // } catch (err) {
+    //     console.log(err)
+    //     return undefined
+    // }
+}
+
+
+// Actual HTTP endpoint
+exports.saveFileToCDN = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        let contentString = request.body.content
+        let commitMessage = request.body.message
+        let filePath = request.body.path
+        let userData = {
+            name: "Yadunandan",
+            email: "nandubatchu@gmail.com"
+        }
+
+        saveFileToRepo(contentString, commitMessage, filePath, userData)
+            .then(saveResponse => {
+                response.json({
+                    "result": saveResponse ? "success" : "File already exists. Need to bump the version!",
+                    "html_url": saveResponse,
+                    "cdn_url": `https://cdn.jsdelivr.net/gh/${owner}/${repo}/${filePath}`
+                })
+            })
+    })
+})
+
+
+
+
+
---
+++ public/script.js
@@ -0,0 +1,111 @@
+let cm = document.getElementById('code')
+let componentView = document.getElementById('componentView')
+let refreshEelementButton = document.getElementById('refreshElement')
+let saveToCDN = document.getElementById('saveToCDN')
+let componentInitText = document.getElementById('componentInitText')
+let componentType = document.getElementById('componentType')
+let componentName = document.getElementById('componentName')
+
+const username = 'nandubatchu'
+const PLACEHOLDERS = {
+    COMPONENT_INIT_TEXT: `<pink-button>My Pink Button</pink-button>`,
+    CODE: `import {LitElement, html} from 'https://unpkg.com/lit-element/lit-element.js?module';
+
+class PinkButtonElement extends LitElement {
+
+    static get properties() {
+        return {}
+    }
+
+    render() {
+        return html\`
+            <style>
+                button {
+                    background: pink;
+                }
+            </style>
+            
+            <button @click=\${() => {console.log("You clicked on the pink button!")}}>
+                <slot></slot>
+            </button>
+        \`;
+    }  
+}
+
+customElements.define('pink-button', PinkButtonElement);`
+}
+
+old_session_code = localStorage.getItem("code") ? localStorage.getItem("code") : PLACEHOLDERS.CODE
+componentInitText.value = localStorage.getItem('componentInitText') ? localStorage.getItem("componentInitText") : PLACEHOLDERS.COMPONENT_INIT_TEXT
+
+var myCodeMirror = CodeMirror(document.getElementById("code"), {
+    size: 100,
+    mode: "javascript",
+    lineWrapping: false,
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true,
+    value:old_session_code ? old_session_code : ''
+});
+myCodeMirror.setSize('100%', '100%');
+
+window.onbeforeunload = function() {
+    localStorage.setItem("code", myCodeMirror.getValue());
+    localStorage.setItem("componentInitText", componentInitText.value);
+}
+
+let refreshElement = () => {
+    let userScript = document.getElementById('userScript')
+    let dupScript = document.createElement('script')
+    dupScript.setAttribute('type', 'module')
+    dupScript.setAttribute('id', 'userScript')
+    dupScript.textContent = myCodeMirror.getValue();
+    console.log(dupScript)
+    document.body.replaceChild(dupScript, userScript)
+}
+myCodeMirror.on('changes', () => {
+    var regex = /customElements.define\('(.+)'/;
+    var found = myCodeMirror.getValue().match(regex);
+    if (found[1] != localStorage.getItem('lastElementKey')) {
+        componentInitText.value = `<${found[1]}></${found[1]}>`
+        localStorage.setItem('lastElementKey', found[1])
+    }
+})
+window.onload = () => {
+    refreshElement()
+    componentView.innerHTML = componentInitText.value
+}
+refreshEelementButton.addEventListener('click', () => {
+    // let newEelement = document.createElement(elemkey)
+    // componentView.appendChild(newEelement)
+    componentView.innerHTML = componentInitText.value
+    window.location.reload()
+})
+
+
+let hitFirebaseFunction = () => {
+    // console.log(`testing/${username}/${componentType.value}/${componentName.value}.js`)
+    payload = {
+        "content": myCodeMirror.getValue(),
+        "message": prompt('Tell something about this version update:'),
+        "path": `components/beta/${username}/${componentType.value}/${componentName.value}.js`
+    }
+    return fetch("/saveFileToCDN", {
+        method: "POST",
+        // mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload)
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.cdn_url)
+            alert(`
+            Your file is uploaded to CDN, add the following in <head> tag:
+            
+            <script src='${res.cdn_url}' type='module'></script>`)
+        })
+}
+
+saveToCDN.addEventListener('click', hitFirebaseFunction)
