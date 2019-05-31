77d80501ad9672de574fb471f9940d512b24a654
Yadunandan Batchu <nandubatchu@gmail.com>
2019-06-01 02:04:09 +0530

    fix button text and component references

--- public/components.js
+++ public/components.js
@@ -1,5 +1,3 @@
-// import 'https://cdn.jsdelivr.net/gh/nandubatchu/lit-sink@435e833/components/beta/nandubatchu/0/lit-sink-header.js'
-// import 'https://cdn.jsdelivr.net/gh/nandubatchu/lit-sink@4c690a7/components/beta/nandubatchu/1/lit-sink-header.js'
-import 'https://cdn.jsdelivr.net/gh/nandubatchu/lit-sink@e7cfd4f/components/beta/nandubatchu/2/lit-sink-header.js'
-import 'https://cdn.jsdelivr.net/gh/nandubatchu/lit-sink@acccd0c/components/beta/nandubatchu/0/lit-sink-footer.js'
+import 'https://cdn.jsdelivr.net/gh/lit-sink/lit-sink-components@0.0.3/lit-sink-header.js'
+import 'https://cdn.jsdelivr.net/gh/lit-sink/lit-sink-components@0.0.2/lit-sink-footer.js'
 // import 'https://cdn.jsdelivr.net/gh/lit-sink/components@0.0.2/my-loader.js'
